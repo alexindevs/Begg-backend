0f767fc7b1c561f81b1271614191c97e0061f323
Alexin <alexindevs@gmail.com>
2024-03-19 22:48:53 +0100

    added token middleware

--- src/modules/waitlist/waitlist.routes.ts
+++ src/modules/waitlist/waitlist.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import WaitlistController from './waitlist.controller';
 
-const waitlistRouter = express.Router();
+const WaitlistRouter = express.Router();
 const waitlistController = new WaitlistController();
 
-waitlistRouter.get('/waiter', waitlistController.getWaiterByEmail);
-waitlistRouter.post('/wait', waitlistController.createWaiter);
+WaitlistRouter.get('/waiter', waitlistController.getWaiterByEmail);
+WaitlistRouter.post('/wait', waitlistController.createWaiter);
+
+export default WaitlistRouter;
--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,11 @@ const port = process.env.PORT || 7000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/auth", require("./modules/auth/auth.routes"));
-app.use("/waitlist", require("./modules/auth/waitlist.routes"));
+import AuthRouter from "./modules/auth/auth.routes";
+import WaitlistRouter from "./modules/waitlist/waitlist.routes";
+
+app.use("/auth", AuthRouter);
+app.use("/waitlist", WaitlistRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
